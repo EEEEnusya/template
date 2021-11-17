77f764b9d8a3a65341d8ac65960e32a670a46440
Yana <yafilatovaa@yandex.ru>
2021-11-17 14:15:22 +0300

    done

--- sources/task_1.cpp
+++ sources/task_1.cpp
@@ -5,13 +5,29 @@
 
 // Task 1.
 void multi_2(std::vector<int>& input) {
-    // Fix it.
+  int x;
+  x=size(input);
+    for ( int i=0; i<x;++i)
+  {
+      input[i]= input[i]*2;
+    }
 }
 
 void reduce_a(std::vector<int>& input, int a) {
-    // Fix it.
+  int x;
+  x=size(input);
+  for ( int i=0; i<x;++i)
+  {
+    input[i]=input[i]-a;
+  }
 }
 
 void div_on_first(std::vector<int>& input) {
-    // Fix it.
+  int x;
+  x=size(input);
+    for ( int i=1; i<x;++i)
+  {
+      input[i]=input[i]/input[0];
+    }
+    input[0]=1;
 }
--- sources/task_4.cpp
+++ sources/task_4.cpp
@@ -4,9 +4,27 @@
 
 // Task 4.
 void remove_negative(std::vector<int>& input) {
-    // Fix it.
+  int x;
+  x=size(input);
+    for ( int i=0; i<x;++i)
+  {
+      if(input[i]<0)
+      {
+        auto iter=input.cbegin();
+        input.erase(iter+i);
+        break;
+      }
+    }
 }
 
 void remove_last_even(std::vector<int>& input) {
-    // Fix it.
+    for (int i=size(input); i>0; --i)
+  {
+      if(input[i]%2==0)
+      {
+        auto iter=input.cbegin();
+        input.erase(iter+i);
+        break;
+      }
+    }
 }
